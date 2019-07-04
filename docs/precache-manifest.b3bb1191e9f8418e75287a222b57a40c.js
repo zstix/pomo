5e382bd9bbdb22d228f4151eeff88b2b2363a22f
Zack Stickles <zstickles@gmail.com>
2019-07-04 14:02:19 -0500

    Adding build folder for GH pages

---
+++ docs/precache-manifest.b3bb1191e9f8418e75287a222b57a40c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac3b3ff076450e89f81e6017856488d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f600199565a6e324ce5d",
+    "url": "/static/css/main.9353679d.chunk.css"
+  },
+  {
+    "revision": "f06fcb84e0c8c8d69295",
+    "url": "/static/js/2.36c0fa17.chunk.js"
+  },
+  {
+    "revision": "f600199565a6e324ce5d",
+    "url": "/static/js/main.584fb254.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
